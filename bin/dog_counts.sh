ec2cfee5bf98118f71d039a2a31a7d028cd5f6b8
Dan Hagberg <sweep@digitaltsunami.net>
2019-11-28 12:42:28 -0800

    Add bin files

---
+++ bin/dog_counts.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/usr/local/bin/python3 /Users/dhagberg/dev/seattle_humane/etl_dog_ex_list.py /Users/dhagberg/Downloads/Dog_Exercise_List.csv $1
+
---
+++ bin/shift_counts.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/usr/local/bin/python3 /Users/dhagberg/dev/seattle_humane/shift_counts.py /Users/dhagberg/Downloads/DBS\ Schedule.pdf $1
+
