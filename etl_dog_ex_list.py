88eff49aeca969747a61f8efb045416431895ebc
Dan Hagberg <sweep@digitaltsunami.net>
2019-11-28 12:36:45 -0800

    Initaial commit

---
+++ etl_dog_ex_list.py
@@ -0,0 +1,181 @@
+import csv
+import re
+from argparse import ArgumentParser
+
+import pandas as pd
+import numpy as np
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
+def get_age_in_months(in_text):
+    match = re.search(r'.*/(\d\d)y (\d\d)m', in_text, flags=re.IGNORECASE)
+    age = match.groups() if match else ('0', '0')
+    age_in_months = int(age[0]) * 12 + int(age[1])
+    return age_in_months
+
+
+def get_name(in_text):
+    name = in_text.split(',')[0]
+    name = name.split('-')[0]
+    name = name.split('*')[0]
+    return name
+
+
+def get_weight(in_text):
+    if len(in_text.strip()) > 0:
+        weight = int(in_text.split()[0])
+    else:
+        weight = 0
+    return weight
+
+
+def get_level(row):
+    level = row[4].split()[0].lower() if len(row[4]) > 0 else None
+    if level in ['green', 'blue', 'purple', 'red', 'orange']:
+        level = level.capitalize()
+        if level == 'Red':
+            if re.match(r'.*No Handling Level.*', row[5], re.IGNORECASE):
+                level = 'Red - Default'
+            elif re.match(r'.*Team.*', row[5], re.IGNORECASE):
+                level = 'Red - Team'
+    else:
+        level = None
+    return level
+
+
+def get_dogs(file_loc):
+    dogs = {}
+    with open(file_loc, 'r') as dogs_csv:
+        current_dog = None
+        k_reader = csv.reader(dogs_csv)
+        for row in k_reader:
+            row = row[8:]
+            if row[0] == 'AM':
+                name = get_name(row[5])
+                weight = get_weight(row[7])
+                loc = row[3]
+                age = get_age_in_months(row[6])
+                id = row[8]
+                current_dog = name
+                dogs[current_dog] = {
+                    'id': id,
+                    'weight': weight,
+                    'location': loc,
+                    'age': age,
+                    'holder': False,
+                    'kc': False,
+                    'bite': False,
+                    'Level': None,
+                    'team': False,
+                    'diet': [],
+                    'notes': []
+                }
+            elif row[0] == 'DIET':
+                dogs[current_dog]['diet'].append(row[5])
+            elif len(row) < 2:
+                # End of dog notes
+                current_dog = None
+            elif re.match(r'(.*intake.*|.*incident.*)',
+                          row[4],
+                          flags=re.IGNORECASE):
+                # Intake notes. Skip
+                pass
+            elif row[0] == 'H':
+                dogs[current_dog]['holder'] = True
+            elif 'Bite Quarantine' in row[4]:
+                dogs[current_dog]['bite'] = True
+            elif 'Kennel Cough' in row[4]:
+                dogs[current_dog]['kc'] = True
+            elif row[0] in ['RDR']:
+                # Run dog run.  Skip
+                pass
+            else:
+                level = get_level(row)
+                if level:
+                    dogs[current_dog]['Level'] = level if dogs[current_dog]['bite'] is False else 'Red - BQ'
+                elif current_dog and len(row[5]) > 0:
+                    dogs[current_dog]['notes'].append(row[5])
+
+    dogs = {k: v for k, v in dogs.items() if 'DELETE' not in k}
+
+    return dogs
+
+
+def get_dog_counts_dataframe(dogs_df):
+    aggFunc = {'Level': np.count_nonzero,
+               'holder': sum,
+               'kc': sum
+               }
+
+    dog_counts = dogs_df.groupby(['Level']).agg(aggFunc)
+
+    dog_counts = dog_counts.rename(columns={'Level': 'All', 'holder': 'Holder', 'kc': 'KC'})
+    dog_counts = dog_counts.astype('int32')
+
+    row_total = dog_counts.sum()
+    row_total.name = 'Total'
+    dog_counts = dog_counts.append(row_total)
+    return dog_counts
+
+
+def get_dog_counts_as_html(dogs_df):
+    dog_counts_df = get_dog_counts_dataframe(dogs_df)
+    dc_out = dog_counts_df.rename(index={'Blue': '3 - Blue', 'Green': '2 - Green', 'Orange': '9 - Orange', 'Purple': '4 - Purple',
+                              'Red': '8 - Red', 'Red - BQ': '6 - Red - BQ', 'Red - Default': '7 - Red - Default',
+                              'Red - Team': '5 - Red - Team', 'Total': 'Total'}).sort_index()
+    styles = [
+        dict(selector="tr", props=[('text-align', 'right')]),
+        dict(selector='th', props=[('text-align', 'right')])
+    ]
+    styled = dc_out.style.set_table_styles(styles)
+    return styled.render()
+
+
+def get_dog_info_as_html(dogs_df):
+    return dogs_df[['holder', 'Level', 'location', 'kc']].rename_axis('name') \
+        .sort_values(['holder', 'name'], ascending=[False, True]) \
+        .to_html()
+
+
+def get_dog_dataframe(dogs):
+    dogs_df = pd.DataFrame.from_dict(dogs, orient='index')
+    dogs_df['dbs'] = dogs_df.Level.apply(lambda l: l in ['Green', 'Blue', 'Purple', 'Red - Team'])
+    return dogs_df
+
+
+def filter_for_dbs(dogs_df):
+    return dogs_df[(dogs_df['dbs'] == True)]
+
+
+def filter_for_non_dbs(dogs_df):
+    return dogs_df[(dogs_df['dbs'] == False)]
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('filename', help='File containing dog exercise csv', metavar='dog_file')
+    parser.add_argument('--html', help='Output as html', required=False, action='store_true')
+    args = parser.parse_args()
+    dogs = get_dogs(args.filename)
+    output_html = args.html
+    dogs_df = get_dog_dataframe(dogs)
+    if output_html:
+        print('<h2>DBS Dog Counts</h2>')
+        print(get_dog_counts_as_html(filter_for_dbs(dogs_df)))
+        print('<br><br>')
+        print('<h2>Staff/BPA Dog Counts</h2>')
+        print(get_dog_counts_as_html(filter_for_non_dbs(dogs_df)))
+        print('<br><br>')
+        print('<h2>Dog Locations</h2>')
+        print(get_dog_info_as_html(dogs_df))
+    else:
+        print('DBS Dog Counts')
+        print(get_dog_counts_dataframe(filter_for_dbs(dogs_df)))
+        print('Staff/BPA Dog Counts')
+        print(get_dog_counts_dataframe(filter_for_non_dbs(dogs_df)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ shift_counts.py
@@ -0,0 +1,215 @@
+import re
+from argparse import ArgumentParser
+from datetime import date, datetime, timedelta
+from typing import Tuple
+
+import pandas as pd
+import tabula
+
+shift_times = {
+    1: (((7, 30), (10, 30)), ((13, 0), (16, 0)), ((16, 0), (18, 0))),
+    2: (((7, 30), (10, 30)), ((13, 0), (16, 0)), ((16, 0), (18, 0))),
+    3: (((7, 30), (10, 30)), ((13, 0), (16, 0)), ((16, 0), (18, 0))),
+    4: (((7, 30), (10, 30)), ((13, 0), (16, 0)), ((16, 0), (19, 30))),
+    5: (((7, 30), (10, 30)), ((13, 0), (16, 0)), ((16, 0), (19, 30))),
+    6: (((8, 0), (11, 0)), ((16, 0), (19, 30))),
+    7: (((7, 30), (10, 30)), ((15, 0), (18, 0)))
+}
+
+
+def etl_shift_schedule(file_name: str) -> pd.DataFrame:
+    # tabula.convert_into(file_name, file_name + '.csv', output_format='csv', pages='all', pandas_options={'header': [1]}, lattice=True)
+    # schedule_df = pd.read_csv(file_name + '.csv', header=[1])
+    schedule_df = tabula.read_pdf(file_name, pages='all', pandas_options={'header': [1]}, lattice=True)
+    schedule_df = schedule_df[(schedule_df.Volunteer != 'Volunteer')]  # Remove repeated header rows
+    schedule_df = schedule_df.fillna(method='ffill')
+    schedule_df = schedule_df[(schedule_df.Volunteer != 'Open')]
+    # schedule_df = schedule_df.drop(['Unnamed: 6'], axis=1)
+    return schedule_df
+
+
+def set_level_indicator_vars(schedule_df: pd.DataFrame) -> pd.DataFrame:
+    schedule_df['Blue'] = schedule_df.LEVEL.apply(lambda l: 'BLUE' in l.upper())
+    schedule_df['Purple'] = schedule_df.LEVEL.apply(lambda l: 'PURPLE' in l.upper())
+    return schedule_df
+
+
+def transform_dates(schedule_df: pd.DataFrame) -> pd.DataFrame:
+    schedule_df['Begin'] = schedule_df.apply(lambda l: re.sub(' \(.*\)', '', l['Date']) + ' ' + l['From'], axis=1)
+    schedule_df['Start_Date'] = pd.to_datetime(schedule_df['Begin'], infer_datetime_format=True)
+    schedule_df['End'] = schedule_df.apply(lambda l: re.sub(' \(.*\)', '', l['Date']) + ' ' + l['To'], axis=1)
+    schedule_df['End_Date'] = pd.to_datetime(schedule_df['End'], infer_datetime_format=True)
+    schedule_df = schedule_df.drop(['Begin', 'From', 'End'], axis=1)
+    return schedule_df
+
+
+def get_shift_counts(schedule_df: pd.DataFrame) -> pd.DataFrame:
+    shifts_minimal = schedule_df[['Start_Date', 'End_Date', 'Blue', 'Purple']]
+    shift_counts = shifts_minimal.groupby(['Start_Date', 'End_Date']).sum()
+    return shift_counts
+
+
+def apply_exceptions(schedule_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Apply a hard coded group of exceptions to the schedule attributes
+    Example: Remove from schedule persistent no-shows
+    :param schedule_df: Current schedule
+    :return: updated schedule dataframe with exceptions applied
+    """
+    schedule_df.loc[schedule_df.Volunteer == 'James Kendle', 'Blue'] = True
+    schedule_df.loc[schedule_df.Volunteer == 'James Kendle', 'Purple'] = False
+    return schedule_df
+
+
+def color_need(value):
+    high_need = value > 7
+    highlight = 'color: red;font-weight: bold'
+    return [highlight if h else '' for h in high_need]
+
+
+def color_purples(value):
+    high_need = value < 4
+    highlight = 'color: purple;font-weight: bold'
+    return [highlight if h else '' for h in high_need]
+
+
+def get_shift_counts_as_html(shift_counts_df: pd.DataFrame) -> str:
+    styles = [
+        dict(selector="tr", props=[('text-align', 'right')]),
+        dict(selector='th', props=[('text-align', 'right')])
+    ]
+    styled = shift_counts_df.style \
+        .set_table_styles(styles) \
+        .hide_index() \
+        .apply(color_need, subset=['Need']) \
+        .apply(color_purples, subset=['Purple'])
+
+    return styled.render()
+
+
+def format_shift_counts(assigned: pd.DataFrame) -> pd.DataFrame:
+    a_df = pd.DataFrame(assigned.to_records())  # Flatten out multi index
+    a_df['Date'] = a_df['shift'].apply(shift_date_str)
+    a_df['Time'] = a_df['shift'].apply(shift_time_str)
+    # Drop unneeded columns and reorder
+    a_out = a_df[['Date', 'Time', 'Blue', 'Purple', 'Need']]
+    return a_out
+
+
+def load_and_summarize_counts(file_name: str) -> pd.DataFrame:
+    shifts_df = etl_shift_schedule(file_name)
+    shifts_df = set_level_indicator_vars(shifts_df)
+    shifts_df.to_csv('./bad_data.csv')
+    # print(shifts_df)
+    shifts_df = apply_exceptions(shifts_df)
+    shifts_df = transform_dates(shifts_df)
+    return get_shift_counts(shifts_df)
+
+
+def save_shift_counts_as_html(shift_counts_df: pd.DataFrame, output_file: str):
+    html_text = get_shift_counts_as_html(shift_counts_df)
+    with open(output_file, 'w') as html_out:
+        html_out.write(html_text)
+
+
+def save_shift_counts_as_csv(shift_counts_df: pd.DataFrame, output_file: str):
+    shift_counts_df.to_csv(output_file, index=False)
+
+
+def get_shift_schedule(for_date: date, start: tuple, end: tuple) -> tuple:
+    hour, minute = start
+    start_shift = datetime(for_date.year, for_date.month, for_date.day, hour, minute)
+    hour, minute = end
+    end_shift = datetime(for_date.year, for_date.month, for_date.day, hour, minute)
+    return start_shift, end_shift
+
+
+def generate_schedule(start_date: date, num_of_days=14):
+    schedule = list()
+    for day in range(num_of_days):
+        curr_date = start_date + timedelta(day)
+        shifts = shift_times[curr_date.isoweekday()]
+        for shift in shifts:
+            schedule.append(get_shift_schedule(curr_date, shift[0], shift[1]))
+    return schedule
+
+
+def is_within_timeframe(timeframe: Tuple[datetime, datetime], time: datetime):
+    return timeframe[0] <= time <= timeframe[1]
+
+
+def is_within_or_overlap(timeframe: Tuple[datetime, datetime], shift: Tuple[datetime, datetime]):
+    return is_within_timeframe(timeframe, shift[0]) or is_within_timeframe(timeframe, shift[1])
+
+
+def get_shift_overlap(timeframe: Tuple[datetime, datetime], shift: Tuple[datetime, datetime]) -> float:
+    if not is_within_or_overlap(timeframe, shift):
+        return 0.0
+
+    shift_start, shift_end = shift
+    tf_start, tf_end = timeframe
+    in_start = max(shift_start, tf_start)
+    in_end = min(shift_end, tf_end)
+    tf_duration = tf_end - tf_start
+    in_duration = in_end - in_start
+    overlap = in_duration / tf_duration
+    return overlap
+
+
+def shift_date_str(shift: Tuple[datetime, datetime]) -> str:
+    return shift[0].strftime('%a %-m/%-d')
+
+
+def shift_time_str(shift: Tuple[datetime, datetime]) -> str:
+    return '{} - {}'.format(shift[0].strftime('%-I:%M'), shift[1].strftime('%-I:%M %p'))
+
+
+def assign_to_shift(shift_counts_df: pd.DataFrame, schedule: dict):
+    # Create datafame for standard shifts
+    assigned_counts = shift_counts_df.reset_index()
+    assigned_counts['shift'] = assigned_counts.apply(
+        lambda row: get_shift((row['Start_Date'], row['End_Date']), schedule), axis=1)
+    assigned_counts = assigned_counts.groupby(['shift']).sum()
+    assigned_counts['Need'] = assigned_counts.apply(lambda r: 15 - r['Blue'] - r['Purple'], axis=1)
+    return assigned_counts
+
+
+def get_shift(shift_time: tuple, schedule: list) -> tuple:
+    for slot in schedule:
+        if get_shift_overlap(shift_time, slot) >= .5:
+            return slot
+    else:
+        return None
+
+
+def assign_to_schedule(shift_counts: pd.DataFrame) -> pd.DataFrame:
+    start = shift_counts.index[0][0]
+    end = shift_counts.index[-1][0]
+    num_days = (end - start).days + 1
+    schedule = generate_schedule(start, num_days)
+    return assign_to_shift(shift_counts, schedule)
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('filename', help='File containing DBS Shift report PDF', metavar='dbs_file')
+    parser.add_argument('--html', help='Output as html', required=False, action='store_true')
+    parser.add_argument('--csv', help='Output as csv', required=False, action='store_true')
+    args = parser.parse_args()
+    shift_counts_df = load_and_summarize_counts(args.filename)
+    assigned = assign_to_schedule(shift_counts_df)
+    assigned_fmt = format_shift_counts(assigned)
+    output_html = args.html
+    output_csv = args.csv
+    if output_html:
+        print('<h2>DBS Shift Counts</h2>')
+        print(get_shift_counts_as_html(assigned_fmt))
+    if output_csv:
+        save_shift_counts_as_csv(assigned_fmt, 'DBS_Shift_Counts.csv')
+    if not output_html and not output_csv:
+        print('DBS Shift Counts')
+        print(assigned_fmt)
+
+
+if __name__ == '__main__':
+    main()
